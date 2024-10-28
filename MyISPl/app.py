5244e9863e2f32e06126b72b8305466f4a510f47
m1k3e <dangduchuutoann040506@gmail.com>
2024-10-28 19:44:14 +1300

    change port

--- MyISPl/app.py
+++ MyISPl/app.py
@@ -71,4 +71,4 @@ def test400():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=False, port=8181)
+    socketio.run(app, debug=False, host='0.0.0.0', port=8000)
